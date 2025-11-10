09fe92a47ed6624f63ad5770a69172d194aca5f7
lanaya113505 <98032050+lanaya113505@users.noreply.github.com>
2025-11-10 13:45:12 +0700

    decision-logic.js

--- decision-logic.js
+++ decision-logic.js
@@ -1,10 +1,13 @@
+// decision-logic.js
 function scoreAd(adMetadata, bid, auctionConfig, trustedScoringSignals, browserSignals) {
-  // Semua iklan kita kasih skor 1 biar lolos
+  // Semua ad dikasih skor positif biar lolos
+  console.log("Menjalankan scoreAd untuk:", browserSignals.renderURL);
   return 1;
 }
 
 function reportResult(auctionConfig, browserSignals) {
-  // Logging sederhana
   console.log("Ad menang:", browserSignals.renderURL);
-  return { reportUrl: "https://lanaya113505.github.io/pizza/report.html" };
+  return {
+    reportUrl: "https://lanaya113505.github.io/pizza/report.html"
+  };
 }
